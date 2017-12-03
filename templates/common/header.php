2f4348136cd515e052931ca5f6e3cd7f20a749c7
João Seixas <up201505648@fe.up.pt>
2017-12-03 20:42:10 +0000

    Changed color scheme

--- templates/common/header.php
+++ templates/common/header.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="resources/fonts/S_Hand.ttf" />
     <link rel="stylesheet" href="resources/fonts/The_Blacklist.ttf" />
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" >
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
 
