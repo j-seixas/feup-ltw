1f2bede2fb0d27607518a7b8b1abce685315c703
João Seixas <up201505648@fe.up.pt>
2017-12-03 17:27:36 +0000

    small fix

--- templates/common/header.php
+++ templates/common/header.php
@@ -5,18 +5,16 @@
     <title>To-Do Master</title>
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="fonts/S_Hand.ttf" />
-    <link rel="stylesheet" href="fonts/The_Blacklist.ttf" />
+    <link rel="stylesheet" href="resources/fonts/S_Hand.ttf" />
+    <link rel="stylesheet" href="resources/fonts/The_Blacklist.ttf" />
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" >
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
-  
+
   <body>
     <header>
       <div id= "info">
         <img src="resources/images/logo.svg"/>
         <h1><a href="index.php" style='color:white'>To-Do Master</a></h1>
       </div>
-      <?php include_once('templates/common/login.php'); ?>
     </header>
-
